33c8f455f11c4c116518d58f401b2e9cf6c4c850
Baptiste Goncalves <baptistegoncalves@mbp-de-baptiste.home>
2024-06-29 13:10:48 +0200

    code qui permet de ponder une playlist

---
+++ UpdatePlaylistPondere.py
@@ -0,0 +1,81 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import datetime
+
+# Configuration de l'authentification
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id='c6fc92f83df140619ed25edf460e642b',
+    client_secret='a6e8c5477f8c40c6bb8399603664b148',
+    redirect_uri='http://localhost:8888/callback',
+    scope='playlist-read-private playlist-modify-private'
+))
+
+# Fonction pour obtenir les pistes et leurs dates d'ajout
+def get_playlist_tracks(playlist_id):
+    results = sp.playlist_items(playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+    return tracks
+
+# Pondérer les pistes en fonction de leur date d'ajout
+def weight_tracks(tracks):
+    weighted_tracks = []
+    now = datetime.datetime.now()
+    for item in tracks:
+        track = item['track']
+        if 'id' in track and 'added_at' in item:
+            added_at = datetime.datetime.strptime(item['added_at'], '%Y-%m-%dT%H:%M:%SZ')
+            days_since_added = (now - added_at).days
+            if days_since_added <= 14: # moins de 2 semaines
+                weight = 6
+            elif days_since_added <= 30: # moins d'1 mois
+                weight = 4
+            elif days_since_added <= 120: # moins de 6 mois
+                weight = 3
+            elif days_since_added <= 365: # moins d'1 an
+                weight = 2
+            else: # plus d'un an
+                weight = 1
+            weighted_tracks.extend([track] * weight)
+    print(f"Nombre total de pistes pondérées : {len(weighted_tracks)}")
+    return weighted_tracks
+
+# Diviser la liste en lots pour ne pas dépasser la limite de l'API
+def chunked_list(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i:i + size]
+
+# Actualiser la playlist existante avec les pistes pondérées
+def update_weighted_playlist(tracks, playlist_id):
+    track_ids = [track['id'] for track in tracks]
+    
+    # Effacer les pistes existantes de la playlist
+    sp.playlist_replace_items(playlist_id, [])
+    print("Suppression des musqiues actuelles")
+    
+    # Diviser les track_ids en lots de 100
+    for chunk in chunked_list(track_ids, 100):
+        print(f"Ajout de {len(chunk)} pistes à la playlist.")
+        sp.playlist_add_items(playlist_id, chunk)
+
+# ID de la playlist d'origine et de la playlist pondérée
+original_playlist_id = '6u7lWmNehQJQll0tq3n7eP'  # ID de la playlist d'origine
+weighted_playlist_id = '4oVVWl0XAuTymHN1n6oV8N'  # ID de la playlist à laquelle on veut ajouter les musiques pondérées
+
+# Obtention et pondération des pistes
+tracks = get_playlist_tracks(original_playlist_id)
+print(f"Nombre de pistes récupérées : {len(tracks)}")
+weighted_tracks = weight_tracks(tracks)
+
+# S'assurer que chaque piste est au moins une fois dans la liste pondérée
+unique_tracks = {track['track']['id']: track['track'] for track in tracks if 'id' in track['track']}.values()
+for track in unique_tracks:
+    if track not in weighted_tracks:
+        weighted_tracks.append(track)
+
+# Actualisation de la playlist à laquelle on veut ajouter les musiques pondérées
+update_weighted_playlist(weighted_tracks, weighted_playlist_id)
+
+print("Playlist pondérée actualisée avec succès!")
